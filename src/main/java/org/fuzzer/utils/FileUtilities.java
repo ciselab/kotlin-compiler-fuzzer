a3b328cd32fe0d2c89b91e5779b972fa434c81b8
Calin Georgescu <caling@protonmail.com>
2023-01-29 17:55:32 +0100

    Make charset explicit

--- src/main/java/org/fuzzer/utils/FileUtilities.java
+++ src/main/java/org/fuzzer/utils/FileUtilities.java
@@ -4,10 +4,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class FileUtilities {
     public static String fileContentToString(File file) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
             StringBuilder sb = new StringBuilder();
             String line = br.readLine();
 
